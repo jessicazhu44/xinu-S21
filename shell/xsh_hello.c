e4905268c79f3be80ebe483c75fe48092d53dded
Jessica Zhu <jessicazhu44@gmail.com>
2021-01-25 15:57:36 -0500

    hello fix

--- shell/xsh_hello.c
+++ shell/xsh_hello.c
@@ -9,7 +9,16 @@ shellcmd xsh_hello(int nargs, char *args[])
 		fprintf(stderr, "%s: too many arguments\n", args[0]);
 		return 1;
 	}
-	printf("Hello %s, Welcome to the world of Xinu!!\n", args[1]);
+
+	if (nargs < 2) {
+		fprintf(stderr, "%s: too few arguments\n", args[0]);
+		return 1;
+	}
+
+	if (nargs == 2){
+		printf("Hello %s, Welcome to the world of Xinu!!\n", args[1]);	
+	}
+
 
 	return 0;
 }
