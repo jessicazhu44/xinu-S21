b89ff9a5df3eb28b4bd6977e3aeb50648c19037f
Jessica Zhu <jessicazhu44@gmail.com>
2021-02-09 11:21:13 -0500

    update

--- shell/xsh_run.c
+++ shell/xsh_run.c
@@ -26,11 +26,19 @@ shellcmd xsh_run(int nargs, char *args[]) {
     nargs--;
 
     if(strncmp(args[0], "hello", 5) == 0) {
+    	if (nargs == 1) {
+    		printf("run hello [name]\n");
+    		return 0;
+    	}
       /* create a process with the function as an entry point. */
       resume (create((void *)xsh_hello, 4096, 20, "hello", 2, nargs, args));
       // printf("run hello %s\n", args[0]);
     }
     if(strncmp(args[0], "prodcons", 8) == 0) {
+    	if (nargs == 1) {
+    		printf("run prodcons [counter]\n");
+    		return 0;
+    	}
       /* create a process with the function as an entry point. */
       resume (create((void *)xsh_prodcons, 4096, 20, "prodcons", 2, nargs, args));
       // printf("run prodcons %s\n", args[0]);
